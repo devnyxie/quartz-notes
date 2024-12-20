53affc7a6cf1b1655a1882284ec6f4bd8c35880a
Tim Afanasiev <soupsoupchannel@gmail.com>
2024-12-20 04:22:32 +0100

    Quartz sync: Dec 20, 2024, 4:22 AM

--- quartz.layout.ts
+++ quartz.layout.ts
@@ -28,13 +28,12 @@ export const defaultContentPageLayout: PageLayout = {
     Component.MobileOnly(Component.Spacer()),
     Component.Search(),
     Component.Darkmode(),
-   
     Component.DesktopOnly(Component.Explorer()),
   ],
   right: [
-    // Component.Graph(),
+    Component.Graph(),
     Component.DesktopOnly(Component.TableOfContents()),
-    Component.DesktopOnly(Component.RecentNotes({ showTags: false, limit: 3 })),
+    // Component.DesktopOnly(Component.RecentNotes({ showTags: false, limit: 3 })),
   ],
 }
 
