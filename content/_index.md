ac44c981473d8012f832585811f2ef3572b9860c
Tim Afanasiev <soupsoupchannel@gmail.com>
2024-12-20 04:28:52 +0100

    Quartz sync: Dec 20, 2024, 4:28 AM

--- content/_index.md
+++ content/_index.md
@@ -10,3 +10,6 @@ Welcome! <br/>
 I’m Timothee. This is my little [digital garden](https://jzhao.xyz/posts/networked-thought) on the Internet — make yourself at home!
 
 At the moment, I'm studying C/C++ at 42 Warsaw and planning to specialize in Cybersecurity, building on my solid background in development.
+
+Quick access:
+- [42 Warsaw](/42Warsaw)
--- quartz.layout.ts
+++ quartz.layout.ts
@@ -31,7 +31,7 @@ export const defaultContentPageLayout: PageLayout = {
     Component.DesktopOnly(Component.Explorer()),
   ],
   right: [
-    Component.Graph(),
+    // Component.DesktopOnly(Component.Graph()),
     Component.DesktopOnly(Component.TableOfContents()),
     // Component.DesktopOnly(Component.RecentNotes({ showTags: false, limit: 3 })),
   ],
